3022230da83b8d3958aed094b7fa10a0df2842aa
Kacper Gała <kacper.gala@grupawp.pl>
2023-01-16 09:28:26 +0100

    feat: Update web and docs

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
         <div className="mx-auto mt-5 max-w-xl flex-col sm:flex sm:justify-center md:mt-8">
           <h1 className="mx-auto max-w-5xl mb-3 text-center text-6xl font-extrabold leading-[1.1] tracking-tighter text-white sm:text-7xl lg:text-8xl xl:text-8xl">
-            Web
+            Web 🖥
           </h1>
           <Button>My button</Button>
         </div>
